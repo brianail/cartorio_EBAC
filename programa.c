67e8c94a77dd1909cab7177684412ed919e46da0
Brian Ail <brianaildasilva90@gmail.com>
2023-03-07 19:32:31 -0300

    Opção de saída
    incluso no sistema de salida do programa.

--- programa.c
+++ programa.c
@@ -2,17 +2,18 @@
 #include <stdlib.h> //biblioteca de alocação de espaço de memória
 #include <locale.h> //biblioteca de alocação de texto por região
 #include <string.h> //biblioteca responsavel por cuidar das STRING
-// funções
-int registro()
-{
-    char arquivo[40];
-    char cpf[40];
-    char nome[40];
-    char sobrenome[40];
-    char cargo[40];
 
-    printf("Digite o CPF a ser cadastrado");
-    scanf("%s", cpf);
+// funções
+int registro() // função que cadastra usuarios
+{
+    char arquivo[40];   // definindo as variaveis
+    char cpf[40];       // definindo as variaveis
+    char nome[40];      // definindo as variaveis
+    char sobrenome[40]; // definindo as variaveis
+    char cargo[40];     // definindo as variaveis
+
+    printf("Digite o CPF a ser cadastrado"); // coletando infos
+    scanf("%s", cpf);                        // salva na variavel cpf
 
     strcpy(arquivo, cpf); // responsavel por cuidar os valores das string
 
@@ -57,13 +58,13 @@ int registro()
     system("pause");
 }
 
-int consulta()
+int consulta() // função de consulta
 {
 
     setlocale(LC_ALL, "Portuguese"); // definindo as linguagens
 
-    char cpf[40];
-    char conteudo[200];
+    char cpf[40];       // definindo as variaveis
+    char conteudo[200]; // definindo as variaveis
 
     printf("Digite o CPF a ser consultado");
     scanf("%s", cpf);
@@ -86,19 +87,19 @@ int consulta()
     system("pause");
 }
 
-int deletar()
+int deletar() // função deletar usuario
 {
-    char cpf[40];
+    char cpf[40]; // definindo as variaveis
 
     printf("Digite o CPF a ser deletado:");
     scanf("%s", cpf);
 
-    remove(cpf);
-    
+    remove(cpf); // remove o cpf
+
     FILE *file;
-    file = fopen(cpf,"r");
-    
-    if(file == NULL) 
+    file = fopen(cpf, "r");
+
+    if (file == NULL)
     {
         printf("o usuário não se encontra no sistema!\n");
         system("pause");
@@ -111,7 +112,7 @@ int main()
 {
 
     int opcao = 0; // definindo as variaveis
-    int laco = 1;
+    int laco = 1;  // definindo as variaveis
 
     for (laco = 1; laco = 1;)
     {
@@ -124,6 +125,7 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n\n");
+        printf("\t4 - Sair do sistema\n\n");
         printf("Opção:"); // fim do menu
 
         scanf("%d", &opcao); // armazenando a escolha
@@ -141,6 +143,10 @@ int main()
             break;
         case 3:
             deletar(); // chamando funções
+            break;
+        case 4:
+            printf("Obrigado por utilizar o sistema!\n");
+            return 0;
             break;
         default:
             printf("Essa opção não está disponivel!");
